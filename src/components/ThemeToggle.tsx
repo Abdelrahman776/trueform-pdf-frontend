64cb45e330a226798e84757a6dc522ed51c6222c
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-08-29 17:59:50 +0300

    refactor ThemeToggle component to simplify icon class names and remove unnecessary comment

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -13,11 +13,10 @@ export default function ThemeToggle({ theme, setTheme }: Props) {
       aria-label={`Switch to ${theme === "light" ? "dark" : "light"} mode`}
     >
       {theme === "light" ? (
-        <MoonIcon className="w-5 h-5 text-tfblack dark:text-tfwhite" />
+        <MoonIcon className="w-5 h-5 text-tfblack" />
       ) : (
-        <SunIcon className="w-5 h-5 text-tfblack dark:text-tfwhite" />
+        <SunIcon className="w-5 h-5  dark:text-tfwhite" />
       )}
     </button>
   );
 }
-//there is a bug here when I am in dark mode in wide screen then move to mobile screen the icon is switched even though the mode is still the same
