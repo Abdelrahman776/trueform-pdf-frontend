d11bea74572e61ea10b1ae460c6ac8c7b8814653
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-08-30 08:12:47 +0300

    feat(ui): add BeforeAfter and integrate Hero sections to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 // import { useState } from 'react'
 
-import { Features } from "./components/Features";
-import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+import BeforeAfterSection from "./components/BeforeAfter";
+import Features from "./components/Features";
 import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Features/>
-      <Pricing/>
+      <BeforeAfterSection />
+      <Hero />
+      <Features />
+      <Pricing />
       <Footer />
     </>
   );
