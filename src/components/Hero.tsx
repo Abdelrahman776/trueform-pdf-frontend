6bd21d4f75cc6df1681de52a6e6cb26cab3bbefb
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-08-30 08:12:13 +0300

    feat(ui): add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,30 @@
+// Unlock Your Documents
+// Instantly transform any scanned PDF into a fully searchable, editable, and lightweight file.
+
+
+// Remake your scanned document in its trueform
+// Convert scanned PDFs and handwritten notes into searchable, editable, compressed digital PDFs — powered by OCR and AI.
+
+//     Everything You Need, Nothing You Don't
+// TrueForm PDF is packed with powerful features to make your documents better.
+
+// Smart Conversion
+// Our AI-powered engine intelligently recognizes text, layouts, and tables for perfect conversions every time.
+// Full-Text Searchability
+// Go beyond images. Our OCR makes every word in your document searchable, saving you hours of manual searching.
+// Optimized Compression
+// Get the smallest possible file size without sacrificing quality, making your documents easy to store and share.
+// Handwritten Note Support
+// Even your handwritten notes are captured and made searchable. Never lose a brilliant idea again.
+
+import { UploadCloud } from "lucide-react";
+
+
+export default function Hero() {
+    
+
+
+    return (<section>
+        heroo
+    </section>)
+}
